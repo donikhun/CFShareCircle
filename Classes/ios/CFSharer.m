34e6f32af04ad7812e591aea99d87c5e040068e5
bfcrampton <bryancrampton@me.com>
2014-02-04 00:22:24 -0500

    Update CFSharer.m

--- Classes/ios/CFSharer.m
+++ Classes/ios/CFSharer.m
@@ -10,8 +10,8 @@
 #import "SIAlertView.h"
 #import "CFShareCircleView.h"
 
-#define kFacebookAppID @"xxxxx"
-#define kPinterestClientId @"xxxxx"
+#define kFacebookAppID @"198142280345578"
+#define kPinterestClientId @"1435709"
 
 @implementation CFSharer
 
@@ -79,7 +79,16 @@
     [mailer addAttachmentData:UIImagePNGRepresentation(self.shareView.params[@"image"]) mimeType:@"image/jpeg" fileName:@"SoL_Photo"];
     [mailer setMessageBody:self.shareView.params[@"full_caption"] isHTML:NO];
     [mailer setSubject:self.shareView.params[@"subject"]];
-    [self.shareView.parentViewController presentViewController:mailer animated:YES completion:nil];
+    
+    if (self.shareView.parentViewController.presentedViewController) {
+      [self.shareView.parentViewController dismissViewControllerAnimated:YES completion:^{
+        [self.shareView.parentViewController presentViewController:mailer animated:YES completion:nil];
+      }];
+    }
+    else {
+      [self.shareView.parentViewController presentViewController:mailer animated:YES completion:nil];
+    }
+
     savedCompletionBlock = completionBlock;
   }
   else {
@@ -111,54 +120,76 @@
 }
 
 - (void)shareToFacebook:(Block)completionBlock {
-  ACAccountStore *account = [[ACAccountStore alloc] init];
-  ACAccountType *facebookType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+  if (![SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook]) {
+    [SIAlertView presentErrorWithMessage:@"Could not access Facebook"];
+    return;
+  }
   
-  NSDictionary *options = @{ ACFacebookAppIdKey: kFacebookAppID, ACFacebookPermissionsKey: @[@"publish_stream"], ACFacebookAudienceKey: ACFacebookAudienceFriends };
-
-  [account requestAccessToAccountsWithType:facebookType options:options completion:^(BOOL granted, NSError *error) {
-    if (granted) {
-      NSArray *accounts = [account accountsWithAccountType:facebookType];
-      if (accounts.count == 0) {
-        [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"No facebook accounts available! Please add one in your device settings."];
-      }
-      else {
-        NSURL *url = [NSURL URLWithString:@"https://graph.facebook.com/me/photos"];
-        NSDictionary *params = @{@"message" : @"via www.appstore.com/SocietyOfLacquer"};
-        NSData *imageData = UIImagePNGRepresentation(self.shareView.params[@"image"]);
-
-        SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook
-                                                requestMethod:SLRequestMethodPOST
-                                                          URL:url
-                                                   parameters:params];
-        [request addMultipartData:imageData
-                         withName:@"source"
-                             type:@"multipart/form-data"
-                         filename:@"Image"];
-        
-        [request setAccount:[accounts lastObject]];
-        
-        [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-          if (responseData) {
-            NSInteger statusCode = urlResponse.statusCode;
-            if (statusCode >= 200 && statusCode < 300) {
-              completionBlock();
-            }
-            else {
-              [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] invalid status code: %@", error.localizedDescription]];
-            }
-          }
-          else {
-            [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] uploading post: %@", error.localizedDescription]];
-          }
-          
-        }];
-      }
-    }
-    else {
-      [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"[Error] with account authorization. Please check that you havea facebook account linked in your device settings."];
-    }
+  SLComposeViewController *facebookComposeView = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+  [facebookComposeView setInitialText:@"via #societyoflacquer"];
+  [facebookComposeView addImage:self.shareView.params[@"image"]];
+  
+  [facebookComposeView setCompletionHandler:^(SLComposeViewControllerResult result) {
+    completionBlock();
   }];
+  
+  if (self.shareView.parentViewController.presentedViewController) {
+    [self.shareView.parentViewController dismissViewControllerAnimated:YES completion:^{
+      [self.shareView.parentViewController presentViewController:facebookComposeView animated:YES completion:nil];
+    }];
+  }
+  else {
+    [self.shareView.parentViewController presentViewController:facebookComposeView animated:YES completion:nil];
+  }
+  
+//  ACAccountStore *account = [[ACAccountStore alloc] init];
+//  ACAccountType *facebookType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+//  
+//  NSDictionary *options = @{ ACFacebookAppIdKey: kFacebookAppID, ACFacebookPermissionsKey: @[@"publish_stream"], ACFacebookAudienceKey: ACFacebookAudienceFriends };
+//
+//  [account requestAccessToAccountsWithType:facebookType options:options completion:^(BOOL granted, NSError *error) {
+//    if (granted) {
+//      NSArray *accounts = [account accountsWithAccountType:facebookType];
+//      if (accounts.count == 0) {
+//        [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"No facebook accounts available! Please add one in your device settings."];
+//      }
+//      else {
+//        NSURL *url = [NSURL URLWithString:@"https://graph.facebook.com/me/photos"];
+//        NSDictionary *params = @{@"message" : @"via www.appstore.com/SocietyOfLacquer"};
+//        NSData *imageData = UIImagePNGRepresentation(self.shareView.params[@"image"]);
+//
+//        SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook
+//                                                requestMethod:SLRequestMethodPOST
+//                                                          URL:url
+//                                                   parameters:params];
+//        [request addMultipartData:imageData
+//                         withName:@"source"
+//                             type:@"multipart/form-data"
+//                         filename:@"Image"];
+//        
+//        [request setAccount:[accounts lastObject]];
+//        
+//        [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+//          if (responseData) {
+//            NSInteger statusCode = urlResponse.statusCode;
+//            if (statusCode >= 200 && statusCode < 300) {
+//              completionBlock();
+//            }
+//            else {
+//              [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] invalid status code: %@", error.localizedDescription]];
+//            }
+//          }
+//          else {
+//            [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] uploading post: %@", error.localizedDescription]];
+//          }
+//          
+//        }];
+//      }
+//    }
+//    else {
+//      [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"[Error] with account authorization. Please check that you havea facebook account linked in your device settings."];
+//    }
+//  }];
 }
 //
 #pragma mark - Pinterest
@@ -168,7 +199,12 @@
 
 - (void)shareToPinterest:(Block)completionBlock {
   Pinterest *_pinterest = [[Pinterest alloc] initWithClientId:kPinterestClientId];
-  [_pinterest createPinWithImageURL:self.shareView.params[@"image_url"] sourceURL:self.shareView.params[@"source_url"] description:@"via www.appstore.com/SocietyOfLacuqer"];
+  if ([_pinterest canPinWithSDK]) {
+    [_pinterest createPinWithImageURL:self.shareView.params[@"image_url"] sourceURL:self.shareView.params[@"source_url"] description:@"via www.appstore.com/SocietyOfLacuqer"];
+  }
+  else {
+    [SIAlertView presentErrorWithMessage:@"Please install the Pinterest app!"];
+  }
   completionBlock();
 }
 
@@ -178,52 +214,74 @@
 }
 
 - (void)shareToTwitter:(Block)completionBlock {
-  ACAccountStore *account = [[ACAccountStore alloc] init];
-  ACAccountType *twitterType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+  if (![SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter]) {
+    [SIAlertView presentErrorWithMessage:@"Could not access Facebook"];
+    return;
+  }
   
-  [account requestAccessToAccountsWithType:twitterType options:nil completion:^(BOOL granted, NSError *error) {
-    if (granted) {
-      NSArray *accounts = [account accountsWithAccountType:twitterType];
-      if (accounts.count == 0) {
-        [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"No twitter accounts available! Please add one in your device settings."];
-      }
-      else {
-        NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/update_with_media.json"];
-        NSDictionary *params = @{@"status" : @"via #societyoflacquer"};
-        NSData *imageData = UIImageJPEGRepresentation(self.shareView.params[@"image"], 1.0f);
-        
-        SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
-                                                requestMethod:SLRequestMethodPOST
-                                                          URL:url
-                                                   parameters:params];
-        [request addMultipartData:imageData
-                         withName:@"media[]"
-                             type:@"image/jpeg"
-                         filename:@"image.jpg"];
-        
-        [request setAccount:[accounts lastObject]];
-        
-        [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-          if (responseData) {
-            NSInteger statusCode = urlResponse.statusCode;
-            if (statusCode >= 200 && statusCode < 300) {
-              completionBlock();
-            }
-            else {
-              [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] invalid status code: %@", error.localizedDescription]];
-            }
-          }
-          else {
-            [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] uploading tweet: %@", error.localizedDescription]];
-          }
-          
-        }];
-      }
-    }
-    else {
-      [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"[Error] with account authorization. Please check that you havea twitter account linked in your device settings."];
-    }
+  SLComposeViewController *twitterComposeView = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+  [twitterComposeView setInitialText:@"via #societyoflacquer"];
+  [twitterComposeView addImage:self.shareView.params[@"image"]];
+  
+  [twitterComposeView setCompletionHandler:^(SLComposeViewControllerResult result) {
+    completionBlock();
   }];
+  
+  if (self.shareView.parentViewController.presentedViewController) {
+    [self.shareView.parentViewController dismissViewControllerAnimated:YES completion:^{
+      [self.shareView.parentViewController presentViewController:twitterComposeView animated:YES completion:nil];
+    }];
+  }
+  else {
+    [self.shareView.parentViewController presentViewController:twitterComposeView animated:YES completion:nil];
+  }
+
+//  ACAccountStore *account = [[ACAccountStore alloc] init];
+//  ACAccountType *twitterType = [account accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+//  
+//  [account requestAccessToAccountsWithType:twitterType options:nil completion:^(BOOL granted, NSError *error) {
+//    if (granted) {
+//      NSArray *accounts = [account accountsWithAccountType:twitterType];
+//      if (accounts.count == 0) {
+//        [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"No twitter accounts available! Please add one in your device settings."];
+//      }
+//      else {
+//        NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/update_with_media.json"];
+//        NSDictionary *params = @{@"status" : @"via #societyoflacquer"};
+//        NSData *imageData = UIImageJPEGRepresentation(self.shareView.params[@"image"], 1.0f);
+//        
+//        SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
+//                                                requestMethod:SLRequestMethodPOST
+//                                                          URL:url
+//                                                   parameters:params];
+//        [request addMultipartData:imageData
+//                         withName:@"media[]"
+//                             type:@"image/jpeg"
+//                         filename:@"image.jpg"];
+//        
+//        [request setAccount:[accounts lastObject]];
+//        
+//        [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+//          if (responseData) {
+//            NSInteger statusCode = urlResponse.statusCode;
+//            if (statusCode >= 200 && statusCode < 300) {
+//              completionBlock();
+//            }
+//            else {
+//              [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] invalid status code: %@", error.localizedDescription]];
+//            }
+//          }
+//          else {
+//            [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:[NSString stringWithFormat:@"[Error] uploading tweet: %@", error.localizedDescription]];
+//          }
+//          
+//        }];
+//      }
+//    }
+//    else {
+//      [self.shareView.delegate shareCircleView:self.shareView didEncounterErrorMessage:@"[Error] with account authorization. Please check that you havea twitter account linked in your device settings."];
+//    }
+//  }];
 }
 
 //#pragma mark - GoogleDrive
